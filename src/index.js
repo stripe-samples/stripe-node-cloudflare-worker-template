3d6b97bac414532debb62329eb8628aac9db9262
Hidetaka Okamoto <hideokamoto@stripe.com>
2023-09-21 14:12:59 +0900

    fix: typo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const Stripe = require("stripe");
 
 async function handleRequest(request, env) {
-  const stripe = Stripe(env.STRIPE_API_KEYY, {
-    apiVersion: '2023-08-16',
+  const stripe = Stripe(env.STRIPE_API_KEY, {
     appInfo: { // For sample support and debugging, not required for production:
       name: 'stripe-samples/stripe-node-cloudflare-worker-template',
       version: '0.0.1',
